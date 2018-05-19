d8e0f80895357241ad5185485a94ea2a4c74f9aa
Shivam01912 <shivam01912@gmail.com>
2018-05-19 15:26:52 +0530

    improved stt added tts

--- shivam/stt.py
+++ shivam/stt.py
@@ -2,6 +2,8 @@ import speech_recognition as sr
 import time
 
 r=sr.Recognizer()
+r.energy_threshold = 4000
+
 mic = sr.Microphone()
 
 print "Starting to record in 3 secs"
@@ -9,7 +11,8 @@ time.sleep(3)
 
 print "Start speaking"
 with mic as source:
-	audio = r.listen(source)
+	# r.adjust_for_ambient_noise(source)
+	audio = r.record(source,duration=10)
 
 print "Recording ended...Recognizing Speech"
 
